ef421beb4b481fccbae98b3a4afbc92f27fc8a2f
FJG-KEI <kei.gds.pmqhk@gmail.com>
2025-07-02 11:56:47 +0900

    kadai_014をアップロード

---
+++ kadai_014/square.js
@@ -0,0 +1,5 @@
+const hundred = (num) => {
+  return num * 10;
+}
+
+console.log(hundred(10));
--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,6 @@
-const date = new Date();
-const [month, day, year] = [
-  date.getMonth(),
-  date.getDate(),
-  date.getFullYear(),
-];
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1; // 月は0から始まるため+1する
+const date = today.getDate();
 
-console.log(`${year}年${month + 1}月${day}日`);
+console.log(year + "年" + month + "月" + date + "日");
